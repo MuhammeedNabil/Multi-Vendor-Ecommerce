0982a38cb7e75463b65e607f4360be1c9e409132
MuhammeedNabil <mohammednabil1910@gmail.com>
2024-10-12 21:31:45 +0300

    adjust dimentions and menu for Navbar mobile screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import Menu from "./Menu";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      {/* Mobile Screens */}
+      <div className="h-full flex items-center justify-between">
+        <Link href={"/"}>
+          <div className="text-2xl tracking-wide">TechTrove</div>
+        </Link>
+        <Menu />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
