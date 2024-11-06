8a4c06161c9b9bd2c838c44d7a52607c78d5609f
MuhammeedNabil <mohammednabil1910@gmail.com>
2024-11-06 15:37:15 +0200

    add rest file with changes

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,7 +1,7 @@
-import Add from "@/components/PD-page-components/Add";
-import CustomizeProducts from "@/components/PD-page-components/CustomizeProducts";
-import ProductImages from "@/components/PD-page-components/ProductImages";
-import Reviews from "@/components/PD-page-components/Reviews";
+import Add from "@/components/Add";
+import CustomizeProducts from "@/components/CustomizeProducts";
+import ProductImages from "@/components/ProductImages";
+import Reviews from "@/components/Reviews";
 import { wixClientServer } from "@/lib/wixClientServer";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
@@ -68,7 +68,7 @@ const SinglePage = async ({ params }: { params: { slug: string } }) => {
         {/* REVIEWS */}
         <h1 className="text-2xl">User Reviews</h1>
         <Suspense fallback="Loading...">
-          <Reviews productId={product._id!} />
+          <Reviews categoryId={product._id!} />
         </Suspense>
       </div>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,39 @@
 import Link from "next/link";
-import React from "react";
-import Menu from "./navbar-component/Menu";
+import Menu from "./Menu";
 import Image from "next/image";
-import NavIcons from "./navbar-component/NavIcons";
-import SearchBar from "./navbar-component/SearchBar";
+import SearchBar from "./SearchBar";
+import dynamic from "next/dynamic";
+// import NavIcons from "./NavIcons";
+
+const NavIcons = dynamic(() => import("./NavIcons"), { ssr: false });
 
 const Navbar = () => {
   return (
     <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
-      {/* Mobile Screens */}
+      {/* MOBILE */}
       <div className="h-full flex items-center justify-between md:hidden">
-        <Link href={"/"}>
-          <div className="text-2xl tracking-wide">TechTrove</div>
+        <Link href="/">
+          <div className="text-2xl tracking-wide">LAMA</div>
         </Link>
         <Menu />
       </div>
-      {/* Bigger Screens */}
-      <div className="hidden md:flex items-center h-full justify-between gap-8">
-        {/* left */}
+      {/* BIGGER SCREENS */}
+      <div className="hidden md:flex items-center justify-between gap-8 h-full">
+        {/* LEFT */}
         <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
-          <Link href={"/"} className="flex items-center gap-3">
-            <Image src={"/logo.png"} alt="logo" width={24} height={24} />
-            <div className="text-2xl tracking-wide">TechTrove</div>
+          <Link href="/" className="flex items-center gap-3">
+            <Image src="/logo.png" alt="" width={24} height={24} />
+            <div className="text-2xl tracking-wide">LAMA</div>
           </Link>
           <div className="hidden xl:flex gap-4">
-            <Link href={'/'}>HomePage</Link>
-            <Link href={'/shop'}>Shop</Link>
-            <Link href={'/deals'}>Deals</Link>
-            <Link href={'/about'}>About</Link>
-            <Link href={'/contact'}>Contact</Link>
+            <Link href="/">Homepage</Link>
+            <Link href="/">Shop</Link>
+            <Link href="/">Deals</Link>
+            <Link href="/">About</Link>
+            <Link href="/">Contact</Link>
           </div>
         </div>
-        {/* right */}
+        {/* RIGHT */}
         <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8">
           <SearchBar />
           <NavIcons />
